b3cf118960fbf11625d46c0b7c5f8a9608ea72d7
Riels <289770334@qq.com>
2017-11-02 10:53:16 +0800

    3a 3b
    plot 3a & create 3b

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-import numpy as np
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#画出平均年龄分布图像
 import matplotlib.pyplot as plt
+import xlrd
+import math
+from xlrd import open_workbook
+
+min_age = 0
+max_age = 70
+x_data=range(min_age,max_age,1)
+y_data=[0 for i in range(min_age,max_age)]
+x_volte=[]
+temp=[]
+wb = open_workbook('/media/rongliangzi/新加卷/学习资料/研一/大数据分析/hw1/data.xlsx')
 
 
-x = np.linspace(0, 10, 500)
-dashes = [10, 5, 100, 5]  # 10 points on, 5 off, 100 on, 5 off
+worksheet = wb.sheet_by_index(0)
+cols = worksheet.col_values(6);
+for num in range(1,len(cols)):
+    y_data[int(math.floor(cols[num]))]+=1
+plt.plot(x_data, y_data, 'bo-',linewidth=1)
+plt.title(u"avrage age distribution")
+plt.legend()
 
-fig, ax = plt.subplots()
-line1, = ax.plot(x, np.sin(x), '--', linewidth=2,
-                 label='Dashes set retroactively')
-line1.set_dashes(dashes)
+plt.xlabel(u"avrage age")
+plt.ylabel(u"number")
 
-line2, = ax.plot(x, -1 * np.sin(x), dashes=[30, 5, 10, 5],
-                 label='Dashes set proactively')
 
-ax.legend(loc='lower right')
 plt.show()
+print 'over!'
